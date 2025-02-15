03d55afe0f387d05067e0daddab438f04ed7b74c
reamkf <68076678+reamkf@users.noreply.github.com>
2025-02-15 23:50:17 +0900

    Solve incompatible configs

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	output: "export",
 	images: {
+		unoptimized: true,
 		remotePatterns: [
 			{
 				hostname: "image01.seesaawiki.jp",
