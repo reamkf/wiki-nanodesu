e5e0c5febb9cd7ebf7423374a11c97dcfafb0b98
reamkf <68076678+reamkf@users.noreply.github.com>
2025-02-16 12:22:21 +0900

    Adjust layout

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -29,7 +29,7 @@ export function Sidebar() {
 			top-[63px] left-0
 			z-30 md:z-auto
 
-			w-64 md:w-[18rem]
+			w-[18rem]
 			h-[24rem] md:h-auto
 			flex-shrink-0
 
@@ -41,20 +41,22 @@ export function Sidebar() {
 			rounded-lg
 		`}>
 			<nav className="space-y-1 p-4 md:p-0">
-				<div className="flex justify-center items-center block bg-green-200 hover:bg-green-300 rounded-lg mb-2">
+				<div className="flex justify-center items-center block bg-green-200 hover:bg-green-300 hover:underline rounded-lg mb-2">
 					<Image
 						src="https://reamkf.github.io/wiki-nanodesu/no_green.png"
 						alt="「の」のアイコン"
 						width={48}
 						height={48}
-						className="p-3 pr-1 sm:w-[84px] sm:h-[84px]"
+						className="p-3 pr-1 w-[80px] h-[80px] flex-1"
 					/>
 					<Link
 						href="https://seesaawiki.jp/kemono_friends3_5ch/"
-						className="font-bold hover:underline text-green-700 p-3 pl-1"
+						className="font-bold text-green-700 p-3 pl-1 flex-grow"
+						target="_blank"
+						rel="noopener noreferrer"
 						onClick={close}
 					>
-						アプリ版けものフレンズ３Wikiなのだ！
+						アプリ版けものフレンズ３wikiなのだ！
 					</Link>
 				</div>
 				<ul className="list-disc pl-6">
