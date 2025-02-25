47fa044edda6b07a4a1930aedba63dada1eb33b5
reamkf <68076678+reamkf@users.noreply.github.com>
2025-02-25 20:47:35 +0900

    Revert deletion of BasicStatus

--- src/types/common.ts
+++ src/types/common.ts
@@ -4,3 +4,12 @@ export interface ColumnMeta {
 	align: AlignType;
 	width?: string;
 }
+
+
+export interface BasicStatus {
+	kemosute?: number | null;
+	hp: number | null;
+	atk: number | null;
+	def: number | null;
+	estimated?: boolean;
+}
