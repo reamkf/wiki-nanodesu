18db9d1e3840ee58a0870ec3e413a8416ebd6ae3
reamkf <68076678+reamkf@users.noreply.github.com>
2025-02-27 11:57:35 +0900

    Fix link

--- src/app/friends-skills/page.tsx
+++ src/app/friends-skills/page.tsx
@@ -1,7 +1,7 @@
 import { getSkillsWithFriendsData, getEffectTypes } from "@/utils/friendsSkillsData";
 import ClientTabs from "./pageClinet";
 import { PageTitle } from '@/components/PageTitle';
-
+import Link from "next/link";
 import { Metadata } from 'next'
 export const metadata: Metadata = {
 	title: "スキル別フレンズ一覧 - アプリ版けものフレンズ３wikiなのです🦉",
@@ -20,7 +20,16 @@ export default async function FriendsSkillsPage() {
 
 			<p className="p-1">
 				フレンズのスキルを種類ごとにリスト化しています。<br />
-				ただし、自身のみの与ダメージ増加は省略しています。また、状態異常・状態変化関連は状態異常のページと被るため、このページではまとめていません。<br />
+				ただし、自身のみの与ダメージ増加は省略しています。また、状態異常・状態変化関連は
+				<Link
+					href="https://seesaawiki.jp/kemono_friends3_5ch/d/%BE%F5%C2%D6%B0%DB%BE%EF"
+					className="hover:underline"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					状態異常のページ
+				</Link>
+				と被るため、このページではまとめていません。<br />
 				けものミラクルのものは、全てLv.5での効果を記載しています。
 			</p>
 
