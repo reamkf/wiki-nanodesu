bd6d8d64cbe3c10af514c18cdcdca21c9319e936
reamkf <68076678+reamkf@users.noreply.github.com>
2025-03-13 19:31:33 +0900

    Remove unused import

--- src/utils/friends/friends.ts
+++ src/utils/friends/friends.ts
@@ -1,4 +1,4 @@
-import { FriendsAttribute, FriendsAttributeOrder, FriendsDataRow } from "@/types/friends";
+import { FriendsAttribute, FriendsAttributeOrder } from "@/types/friends";
 
 export function sortFriendsAttribute(attributeA: FriendsAttribute, attributeB: FriendsAttribute): number {
 	return FriendsAttributeOrder[attributeA] - FriendsAttributeOrder[attributeB];
