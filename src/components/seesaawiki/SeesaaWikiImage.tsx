7307ff5a33aee7a393f722fac1e73e5072e81a53
reamkf <68076678+reamkf@users.noreply.github.com>
2025-03-17 16:10:55 +0900

    Show nainoda icon if src passed to SeesaaWikiImage is blank

--- src/components/seesaawiki/SeesaaWikiImage.tsx
+++ src/components/seesaawiki/SeesaaWikiImage.tsx
@@ -3,7 +3,7 @@ import Image, { ImageProps }from "next/image";
 export function SeesaaWikiImage(props: ImageProps) {
 	return (
 		<Image
-			src={props.src}
+			src={props.src || 'https://image01.seesaawiki.jp/k/h/kemono_friends3_5ch/hokQFI6JEh.png'}
 			alt={props.alt}
 			width={props.width}
 			height={props.height}
