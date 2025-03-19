51287132e12dbc32f11be991fffd8d6365a33f7b
reamkf <68076678+reamkf@users.noreply.github.com>
2025-03-19 11:10:05 +0900

    Update disclaimer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
 		<footer>
 			<div className="flex flex-col items-center justify-center my-8">
 				<p className="text-xs text-gray-400 max-w-[90%] text-center">
-					ゲーム内や公式Twitterからの画像・文章等の著作権等は「けものフレンズプロジェクト２Ｇ」及び「SEGA」「アピリッツ」又はその関連団体に帰属します。
+					ゲーム内、<a href="https://x.com/kemono_friends3" target="_blank" rel="noopener noreferrer">公式X</a>または<a href="https://kemono-friends-3.jp" target="_blank" rel="noopener noreferrer">公式サイト</a>に由来する画像・文章等の著作権は、「けものフレンズプロジェクト２Ｇ」及び「SEGA」「アピリッツ」又はその関連団体に帰属します。
 				</p>
 				<div className="flex gap-2 text-sm text-gray-500 mt-4 items-center">
 					<Link href="/">アプリ版けものフレンズ３wikiなのです🦉</Link>
