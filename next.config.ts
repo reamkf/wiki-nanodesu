fe5cc40db82201f1e62eccc885bbee853f6545b8
reamkf <68076678+reamkf@users.noreply.github.com>
2025-04-10 16:33:28 +0900

    Use rspack

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import withRspack from 'next-rspack';
 
 const nextConfig: NextConfig = {
 	output: "export",
@@ -18,4 +19,4 @@ const nextConfig: NextConfig = {
 	},
 };
 
-export default nextConfig;
+export default withRspack(nextConfig);
