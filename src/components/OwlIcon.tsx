21b56c2bdc341ff7a259318606559c2496515872
reamkf <68076678+reamkf@users.noreply.github.com>
2025-04-30 22:38:11 +0900

    Fix line break before owl icon

--- src/components/OwlIcon.tsx
+++ src/components/OwlIcon.tsx
@@ -6,7 +6,7 @@ export function OwlIcon() {
 		<>
 			<Image
 				draggable="false"
-				className="ext-emoji"
+				className="inline-block ext-emoji"
 				alt="フクロウ"
 				src="https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/svg/1f989.svg"
 				width={20} height={20}
